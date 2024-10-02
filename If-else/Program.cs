a13ee85cecdfbd36b7b35821c101b9d22e74ff2b
iremrakicii <iremrakicii53@gmail.com>
2024-10-02 11:05:55 +0300

    Update Program.cs

--- If-else/Program.cs
+++ If-else/Program.cs
@@ -7,30 +7,30 @@ if (sayi >= 10)
 {
     if (sayi == 10)
     {
-        Console.WriteLine("Girilen sayı 10'dur");
+        Console.WriteLine("Girilen sayı 10'dur.");
     }
     else if (sayi < 10)
         {
-            Console.WriteLine("Girilen sayı 10'dan küçüktür");
+            Console.WriteLine("Girilen sayı 10'dan küçüktür.");
         }
         else
         {
-            Console.WriteLine("Girilen sayı 10'dan büyüktür");
+            Console.WriteLine("Girilen sayı 10'dan büyüktür.");
         }
     
 }
 else
 {
-    Console.WriteLine("Girilen sayı sıfırdır");
+    Console.WriteLine("Girilen sayı sıfırdır.");
 }
 
 if (sayi > 0)
 {
-    Console.WriteLine("Girilen sayı çifttir");
+    Console.WriteLine("Girilen sayı çifttir.");
 }
 else
 {
-    Console.WriteLine("Girilen sayı tektir");
+    Console.WriteLine("Girilen sayı tektir.");
 }
    
 
