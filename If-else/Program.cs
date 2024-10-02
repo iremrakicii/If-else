aeba62036b26913411dddcd9d4be7c8fa8c4c05b
iremr <iremr@LAKICI53>
2024-10-02 10:56:46 +0300

    Proje dosyası ekle.

---
+++ If-else/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Lütfen bir sayı giriniz:");
+Console.WriteLine("22");
+
+int sayi = 22;
+
+if (sayi >= 10)
+{
+    if (sayi == 10)
+    {
+        Console.WriteLine("Girilen sayı 10'dur");
+    }
+    else if (sayi < 10)
+        {
+            Console.WriteLine("Girilen sayı 10'dan küçüktür");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı 10'dan büyüktür");
+        }
+    
+}
+else
+{
+    Console.WriteLine("Girilen sayı sıfırdır");
+}
+
+if (sayi > 0)
+{
+    Console.WriteLine("Girilen sayı çifttir");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir");
+}
+   
+
